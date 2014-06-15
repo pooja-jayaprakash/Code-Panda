fa2af4dab903ac28a430d90887cba5753e241f9e
Pooja Jeyaprakash <poojajeyaprakash@gmail.com>
2014-06-15 06:55:45 -0400

    SearchBST

--- BinarySearchTree/TreeNode.rb
+++ BinarySearchTree/TreeNode.rb
@@ -1,6 +1,7 @@
 class TreeNode
- attr_accessor :rightchild,:leftchild,:data
-def initialize(data)
-@data = data
+ 	attr_accessor :rightchild,:leftchild,:data
+	def initialize(data)
+		@data = data
+	end
 end
 
